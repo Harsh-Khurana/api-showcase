917235ae723ac24c3ee0265935838680ffe268b8
Harsh-Khurana <harshkhuranaharshkh@icloud.com>
2020-05-30 17:11:55 +0530

    app without postnewdata

--- scripts.js
+++ scripts.js
@@ -4,18 +4,6 @@ const allData = document.getElementById('all-data');
 const formTitle = document.getElementById('form-title');
 const formBody = document.getElementById('form-body');
 
-const newData = (titleText,bodyText) =>{
-	const title = document.createElement('DIV');
-	title.innerHTML=titleText;
-	title.className = 'card-title';
-	const body = document.createElement('DIV');
-	body.innerHTML = bodyText;
-	body.className = 'card-body';
-	const main = document.createElement('DIV');
-	main.className = 'card';
-	main.appendChild(title);	main.appendChild(body);
-	allData.appendChild(main);
-}
 
 // Fn that fetches the data on click of fetch btn and also adds the 
 // data to the page to display
@@ -25,7 +13,16 @@ fetchbtn.addEventListener('click',()=>{
 		.then(result=>{
 			const posts = result.splice(0,10);
 			posts.map(post=>{
-				newData(post.title,post.body);
+				const title = document.createElement('DIV');
+				title.innerHTML=post.title;
+				title.className = 'card-title';
+				const body = document.createElement('DIV');
+				body.innerHTML = post.body;
+				body.className = 'card-body';
+				const main = document.createElement('DIV');
+				main.className = 'card';
+				main.appendChild(title);	main.appendChild(body);
+				allData.appendChild(main);
 			})
 		})
 		.catch(err=>{
@@ -51,7 +48,6 @@ postbtn.addEventListener('click',()=>{
 	  .then(resp => resp.json())
 	  .then(result=>{
 	  		alert("Data submitted successfully !!!")
-	  		newData(formTitle.value,formBody.value)
 	  		console.log(result);
 	  	})
 	  .catch(err=>{
